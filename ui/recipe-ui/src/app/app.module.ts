e028fba9e07f88602574b2e079578e1a5dd1e60e
Bob Malouf <bmalouf@deloitte.com>
2019-11-15 07:19:20 -0500

    comitting ui

--- ui/recipe-ui/src/app/app.module.ts
+++ ui/recipe-ui/src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatGridListModule } from "@angular/material";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { HomeNavComponent } from './home-nav/home-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeLandingComponent } from './recipe-landing/recipe-landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeNavComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeLandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ui/recipe-ui/src/app/recipe-list/recipe-list.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { RecipeListComponent } from './recipe-list.component';
+
+describe('RecipeListComponent', () => {
+  let component: RecipeListComponent;
+  let fixture: ComponentFixture<RecipeListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipeListComponent ],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipeListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
